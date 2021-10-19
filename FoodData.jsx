86b70755663c85a4e34db223ce50f45bdedb1012
gobinda lugun <gobinknightryder098@gmail.com>
2021-10-19 14:54:21 +0530

    Added a new Project

---
+++ FoodData.jsx
@@ -0,0 +1,32 @@
+const FoodData = [
+  {
+    id: '',
+    name: '',
+    image: '',
+    content: ''
+
+  },
+  {
+    id: '',
+    name: '',
+    image: '',
+    content: ''
+
+  },
+  {
+    id: '',
+    name: '',
+    image: '',
+    content: ''
+
+  },
+  {
+    id: '',
+    name: '',
+    image: '',
+    content: ''
+
+  },
+];
+
+export default FoodData;
