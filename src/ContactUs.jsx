e9b540af0a24991f6b673ad22c486971bd68f123
gobinda lugun <gobinknightryder098@gmail.com>
2021-10-19 14:57:42 +0530

    anchor tags

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -5,11 +5,11 @@ const ContactUs = () => {
     <>
       <div className='main-body'>
         <h1>Gobinda Chandra Lugun</h1>
-        <a href="http://github.com/knightryder098/"> GITHUB</a>
+        <a href="http://github.com/knightryder098/" target='_lola'> GITHUB</a>
         <br />
-        <a href="http://linkedin.com/in/gobinda-lugun-5833a485/"> Linkdin</a>
+        <a href="http://linkedin.com/in/gobinda-lugun-5833a485/" target="_john"> Linkdin</a>
         <br />
-        <a href="mailto:gobinknightryder098@gmail.com "> GMAIL</a>
+        <a href="mailto:gobinknightryder098@gmail.com " target='_sonu'> GMAIL</a>
         <br />
       </div>
     </>
